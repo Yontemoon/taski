5211a065e1e6a7fe645ea29445af5907ceb8c92c
YonteMoon <monteyoon1998@gmail.com>
2024-12-28 11:08:26 -0800

    Added tailwind / shadcn

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Button } from "./ui/button";
 
 type PropTypes = {
   children: React.ReactNode;
 };
 
 const Header = ({ children }: PropTypes) => {
-  return <h1 className="text-xl font-bold "> {children}</h1>;
+  return (
+    <>
+      <h1 className=" font-bold text-destructive"> {children}</h1>
+      <Button>Testing</Button>
+    </>
+  );
 };
 
 export default Header;
