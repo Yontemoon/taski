bc313498cb7fb143344c426fa5bc570d5f222a28
YonteMoon <monteyoon1998@gmail.com>
2024-12-28 11:43:50 -0800

    added basic supabase query

---
+++ app/lib/supabase.ts
@@ -0,0 +1,31 @@
+import { createClient } from "@supabase/supabase-js";
+import { createServerClient } from "@supabase/ssr";
+import { parseCookies, setCookie } from "vinxi/server";
+
+const SUPABASE_URL = process.env.SUPABASE_URL!;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+export function getSupabaseServerClient() {
+  return createServerClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        // @ts-ignore Wait till Supabase overload works
+        getAll() {
+          return Object.entries(parseCookies()).map(([name, value]) => ({
+            name,
+            value,
+          }));
+        },
+        setAll(cookies) {
+          cookies.forEach((cookie) => {
+            setCookie(cookie.name, cookie.value);
+          });
+        },
+      },
+    }
+  );
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,47 +1,39 @@
-import * as fs from "node:fs";
 import { createFileRoute, useRouter } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/start";
+import { supabase } from "@/lib/supabase";
 
-const filePath = "count.txt";
-
-async function readCount() {
-  return parseInt(
-    await fs.promises.readFile(filePath, "utf-8").catch(() => "0")
-  );
-}
-
-const getCount = createServerFn({
+const getTodos = createServerFn({
   method: "GET",
-}).handler(() => {
-  return readCount();
+}).handler(async () => {
+  const todos = await supabase.from("todos").select("*");
+  return todos.data;
 });
-
-const updateCount = createServerFn({ method: "POST" })
-  .validator((d: number) => d)
-  .handler(async ({ data }) => {
-    const count = await readCount();
-    await fs.promises.writeFile(filePath, `${count + data}`);
-  });
 
 export const Route = createFileRoute("/")({
   component: Home,
-  loader: async () => await getCount(),
+  loader: async () => await getTodos(),
 });
 
 function Home() {
   const router = useRouter();
   const state = Route.useLoaderData();
 
+  if (!state) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
-    <button
-      type="button"
-      onClick={() => {
-        updateCount({ data: 1 }).then(() => {
-          router.invalidate();
-        });
-      }}
-    >
-      Add 1 to {state}?
-    </button>
+    <div>
+      <h1>My Todos</h1>
+      <ul>
+        {state.map((todo) => (
+          <li key={todo.id}>{todo.todo}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
