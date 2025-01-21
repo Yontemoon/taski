4d6bd9b252b26ef324eaa198b05a621376caa40d
Yontemoon <monteyoon1998@gmail.com>
2025-01-20 19:47:18 -0800

    added login page.

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,7 +1,10 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
-import { Login } from "../components/Login";
 import { formatDate } from "@/lib/utils";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { useForm } from "@tanstack/react-form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
 
 export const Route = createFileRoute("/login")({
   beforeLoad: async ({ context }) => {
@@ -13,9 +16,71 @@ export const Route = createFileRoute("/login")({
 });
 
 function LoginComp() {
+  const form = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: ({ value, formApi }) => {
+      console.log(value);
+      formApi.reset();
+    },
+  });
+
   return (
-    <div>
-      <Card />
+    <div className="flex justify-center">
+      <Card>
+        <CardHeader>
+          <CardContent>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                form.handleSubmit();
+              }}
+            >
+              <form.Field
+                name="email"
+                children={(field) => {
+                  return (
+                    <>
+                      <Label>Email:</Label>
+                      <Input
+                        id={field.name}
+                        name={field.name}
+                        value={field.state.value}
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                      />
+                    </>
+                  );
+                }}
+              />
+              <form.Field
+                name="password"
+                children={(field) => {
+                  return (
+                    <>
+                      <Label>Email:</Label>
+                      <Input
+                        type="password"
+                        id={field.name}
+                        name={field.name}
+                        value={field.state.value}
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                      />
+                    </>
+                  );
+                }}
+              />
+              <Button className="w-full" type="submit">
+                Login
+              </Button>
+            </form>
+          </CardContent>
+        </CardHeader>
+      </Card>
     </div>
   );
 }
