eb7a3f2c88690ac0cbcf37676da9c2b0344a3f71
Yontemoon <monteyoon1998@gmail.com>
2025-02-19 15:18:29 -0800

    added link icon dummy component.

---
+++ src/components/link-icon.tsx
@@ -0,0 +1,23 @@
+import { Link } from "@tanstack/react-router";
+import type { LinkProps } from "@tanstack/react-router";
+
+type PropTypes = {
+  children: React.ReactNode;
+} & LinkProps;
+
+const LinkIcon = ({ children, to, ...props }: PropTypes) => {
+  return (
+    <Link
+      to={to}
+      className="p-3 inline-flex rounded-lg items-center hover:bg-foreground/10 transition-all duration-100"
+      activeProps={{
+        className: "border border-2 border-foreground rounded-lg",
+      }}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default LinkIcon;
