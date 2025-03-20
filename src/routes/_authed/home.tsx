d5eaf00832373734438c5e4faedaaef015218c31
Yontemoon <monteyoon1998@gmail.com>
2025-03-20 11:14:26 -0700

    removed all tags in home section for better load time

--- src/routes/_authed/home.tsx
+++ src/routes/_authed/home.tsx
@@ -3,8 +3,6 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 import YearGrid from "@/components/year-grid";
 import Loader from "@/components/loader";
-import { tagsAllQueryOptions } from "@/lib/options";
-import { useQuery } from "@tanstack/react-query";
 
 export const Route = createFileRoute("/_authed/home")({
   component: RouteComponent,
@@ -15,14 +13,11 @@ function RouteComponent() {
   if (!auth.user) {
     throw redirect({ to: "/login" });
   }
-  const { data: tags } = useQuery(tagsAllQueryOptions(auth.user?.id));
+
   return (
     <div className=" w-full items-center flex flex-col align-middle ">
       <React.Suspense fallback={<Loader />}>
         <YearGrid />
-        {tags?.map((tag) => {
-          return <YearGrid tag={tag.name} key={tag.id} />;
-        })}
       </React.Suspense>
     </div>
   );
